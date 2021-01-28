9c3d4c0c128a258709a7a63985f2e467185971f0
AshleyDhevalall <ashleysdhevalall@yahoo.com>
2021-01-28 20:57:53 +0200

    init commit

---
+++ EmployeeApiClientService/Posts.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace EmployeeApiClientService
+{
+  public class Posts
+  {
+    private readonly HttpClient httpClient;
+    private const string url = "https://jsonplaceholder.typicode.com/posts";
+
+    public Posts(HttpClient httpClient)
+    {
+      this.httpClient = httpClient;
+    }
+
+    public async Task<IEnumerable<JsonElement>> GetPosts()
+    {
+      var response = await httpClient.GetAsync(url);
+      var body = await response.Content.ReadAsStringAsync();
+      var posts = JsonSerializer.Deserialize<IEnumerable<JsonElement>>(body);
+      return posts;
+    }
+
+    public async Task<JsonElement> CreatePost(string title)
+    {
+      var payload = new
+      {
+        title
+      };
+      var httpContent = new StringContent(JsonSerializer.Serialize(payload));
+      var response = await httpClient.PostAsync(url, httpContent);
+      var body = await response.Content.ReadAsStringAsync();
+      var created = JsonSerializer.Deserialize<JsonElement>(body);
+      return created;
+    }
+  }
+}
